a62df84d06eef423975d922e33853edfee29d0e5
Ricky Chan <ric0611@gmail.com>
2020-04-15 03:30:17 -0700

    fixed a bug

--- src/components/PostCardContainer/PostCardContainer.js
+++ src/components/PostCardContainer/PostCardContainer.js
@@ -1,29 +1,37 @@
-import React from 'react'
-import {
-  Container,
-  Grid,
-  Button,
-  Form,
-  Header,
-  Segment,
-  Input,
-  Rating,
-  Image,
-  Divider,
-  Icon,
-  Checkbox,
-  Responsive
-} from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react'
+import db from '../../firestore'
+import firebase from 'firebase'
+// import {
+// } from 'semantic-ui-react'
 import styles from './styles'
 
 import noImage from '../../assets/images/no-image.jpg'
 
 const PostCardContainer = ({ item }) => {
+  const [posterPic, SetPosterPic] = useState('')
+
+  // const getPosterPic = () => {
+  //   let storage = firebase.storage()
+  //   let urlRef = storage.ref('userPic/' + item.posterUid)
+  //   urlRef.getDownloadURL().then(function (downloadURL) {
+  //     // item.refundImg = downloadURL
+  //     return downloadURL
+  //   })
+  //     .then((downloadURL) => {
+  //       updateProfilePic(downloadURL)
+  //       return
+  //     })
+  // }
+  
+  useEffect(()=>{
+    // getPosterPic()
+  },[])
+
   return (
     <div style={styles.container}>
       <img src={item.images.length > 0 ? item.images[0].src : noImage}
         style={styles.img} />
-        <h1>hello</h1>
+      <img src={posterPic} style={styles.posterPic}/>
     </div>
   )
 }
