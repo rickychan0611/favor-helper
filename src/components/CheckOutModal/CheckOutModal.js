d1448d3e5fa6431b56fcaef09db0db9379b70738
Ricky Chan <ric0611@gmail.com>
2020-05-14 15:40:03 -0700

    add datepicker for pickup time

--- src/components/CheckOutModal/CheckOutModal.js
+++ src/components/CheckOutModal/CheckOutModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react'
+import moment from 'moment'
 import {
   Modal,
   Grid,
@@ -8,7 +9,7 @@ import {
   Segment,
   Input,
   Rating,
-  Image,
+  Container,
   Divider,
   Icon,
   Radio,
@@ -18,14 +19,34 @@ import styles from './styles'
 import {
   PriceTimeColumn, Map, PostTitle, PostAboutMeal, PostAboutPoster, QuestionForm, QuestionsContainer
 } from '../../components'
+import {
+  DateInput,
+  TimeInput,
+  DateTimeInput,
+  DatesRangeInput
+} from 'semantic-ui-calendar-react';
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 const CheckOutModal = ({ openModal, setOpenModal, post, poster }) => {
   const [qty, setQty] = useState(1)
   const [step2Modal, setStep2Modal] = useState(false)
+  const [step3aModal, setStep3aModal] = useState(false)
+  const [step3bModal, setStep3bModal] = useState(false)
   const [pickupOrDelivery, setPickupOrDelivery] = useState("Pick-up")
+  const now = moment()
+  const [pickupDate, setPickupDate] = useState(null)
+
+  const addDays = (theDate, days) => {
+    return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
+  }
 
   const handlePickupOrDelivery = (e, { value }) => {
     setPickupOrDelivery(value)
+  }
+
+  const handlePickupDate = (e, { value }) => {
+    setPickupDate(value)
   }
 
   const handleQty = (counter) => {
@@ -90,7 +111,7 @@ const CheckOutModal = ({ openModal, setOpenModal, post, poster }) => {
             </Grid.Column>
             <Grid.Column width={8}>
               <Modal.Description>
-                <h4>
+                <h5>
                   <Header>Please choose one:</Header>
                   <Radio
                     label="Self-pickup"
@@ -102,7 +123,7 @@ const CheckOutModal = ({ openModal, setOpenModal, post, poster }) => {
                   {post.address[2].long_name},&nbsp;
                   {post.address[4].short_name},&nbsp;
                   {post.address[5].short_name}&nbsp;
-              </h4>
+              </h5>
                 <Divider horizontal>Or</Divider>
                 <h4>
                   <Radio
@@ -121,7 +142,7 @@ const CheckOutModal = ({ openModal, setOpenModal, post, poster }) => {
         </Modal.Content>
         <Modal.Actions>
           <Button color='grey' onClick={() => { setStep2Modal(false) }}>
-            <Icon name='close' /> 
+            <Icon name='close' />
           </Button>
           <Button color='green' onClick={() => {
             setOpenModal(true)
@@ -130,15 +151,72 @@ const CheckOutModal = ({ openModal, setOpenModal, post, poster }) => {
             <Icon name='arrow left' />Back
           </Button>
           {pickupOrDelivery === 'Delivery' ?
-            <Button color='green' onClick={() => { setStep2Modal(false) }}>
+            <Button color='green' onClick={() => {
+              setStep2Modal(false)
+              setStep3bModal(true)
+            }}>
               {pickupOrDelivery}<Icon name='arrow right' />
             </Button> :
-            <Button color='green' onClick={() => { setStep2Modal(false) }}>
+            <Button color='green' onClick={() => {
+              setStep2Modal(false)
+              setStep3aModal(true)
+            }}>
               {pickupOrDelivery}<Icon name='arrow right' />
             </Button>
           }
         </Modal.Actions>
       </Modal>
+
+      {/* Step3a: pickup time */}
+      <Modal open={step3aModal} inverted dimmer='blurring'>
+        <Modal.Header>
+          Select a pickup date/time
+        </Modal.Header>
+        <Modal.Content>
+          <Container textAlign='center'>
+            <DatePicker
+              selected={new Date()}
+              onChange={date => setPickupDate(date)}
+              showTimeSelect
+              timeFormat="h:mm aa"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy"
+              inline
+              minDate={new Date()}
+              // maxDate={moment().add(2,'d').toDate()}
+              minTime={now.hours(10).minutes(0).toDate()}
+              maxTime={now.hours(21).minutes(45).toDate()}
+            />
+            <h4>Pickup time:&nbsp;
+            {!pickupDate ? null : pickupDate.toLocaleString('en-US',
+              { weekday: 'short', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true })}
+            </h4>
+          </Container>
+          {/* </Segment> */}
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='grey' onClick={() => { setStep3aModal(false) }}>
+            <Icon name='close' />
+          </Button>
+
+          <Button color='green' onClick={() => {
+            setStep2Modal(true)
+            setStep3aModal(false)
+          }}>
+            <Icon name='arrow left' />Back
+          </Button>
+
+          <Button disabled={!pickupDate} color='green' onClick={() => {
+            setStep3aModal(false)
+            // setStep4aModal(true)
+          }}>
+            Next<Icon name='arrow right' />
+          </Button>
+
+        </Modal.Actions>
+      </Modal>
+
     </>
   )
 }
